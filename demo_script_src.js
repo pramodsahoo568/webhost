b187a5c77ead7691470345892da1787862017db5
pramodsahoo568 <61376710+pramodsahoo568@users.noreply.github.com>
2020-07-10 18:06:41 +0530

    index1.html and js file added

---
+++ demo_script_src.js
@@ -0,0 +1,8 @@
+function randomInt(min, max) {
+	return min + Math.floor((max - min) * Math.random());
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "" + randomInt(1,100);;
+}
+
