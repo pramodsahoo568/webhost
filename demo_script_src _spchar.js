26115c325852076d4b69cafa12fd0b3a540a46dd
pramodsahoo568 <61376710+pramodsahoo568@users.noreply.github.com>
2020-07-14 13:20:02 +0530

    delete

--- demo_script_src _spchar.js
+++
@@ -1,8 +0,0 @@
-﻿function randomInt(min, max) {
-    return min + Math.floor((max - min) * Math.random());
-};   
- 
-function myFunction() {
-    document.getElementById("demo").innerHTML = "" + randomInt(1,100);
-};
- 
