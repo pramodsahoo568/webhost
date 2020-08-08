75de47a7093e2c2be0f6c75d62f75796c932657f
pramodsahoo568 <61376710+pramodsahoo568@users.noreply.github.com>
2020-08-08 10:59:24 +0530

    Update my_linktest.js

--- my_linktest.js
+++ my_linktest.js
@@ -1,9 +1,13 @@
 
+var myObj, x;
+myObj = {"name":"Path", "filename":"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"};
+filepath = myObj.filename;
+
 var a = document.getElementById('link2'); //or grab it by tagname etc
 a.href = "https://in.yahoo.com/?p=us";
 
 var b = document.getElementById('linkimage1'); //or grab it by tagname etc
-b.href = "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg";
+b.href = filepath;
 
 
 function randomInt(min, max) {
