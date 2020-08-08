6c62a86cc213c7c7d5009f137f255a13ec2e8ac0
pramodsahoo568 <61376710+pramodsahoo568@users.noreply.github.com>
2020-08-08 10:47:03 +0530

    Update my_linktest.js

--- my_linktest.js
+++ my_linktest.js
@@ -2,10 +2,9 @@
 var a = document.getElementById('link2'); //or grab it by tagname etc
 a.href = "https://in.yahoo.com/?p=us";
 
-var a = document.getElementById('linkimage1'); //or grab it by tagname etc
-a.href = "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg";
+var b = document.getElementById('linkimage1'); //or grab it by tagname etc
+b.href = "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg";
 
-https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg
 
 function randomInt(min, max) {
 	return min + Math.floor((max - min) * Math.random());
