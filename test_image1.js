65bdeab57536c45cc3b2cf3959990d6f111ddc68
pramodsahoo568 <61376710+pramodsahoo568@users.noreply.github.com>
2020-09-05 19:35:52 +0530

    Update test_image1.js

--- test_image1.js
+++ test_image1.js
@@ -1,8 +1,8 @@
 //var x = document.getElementsByClassName("test_image1");
 //x[0].setAttribute("src", "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg");
-
+  var svgpath = "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg";
   var x = document.createElement("IMG");
-  x.setAttribute("src", "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg");
+  x.setAttribute("src", svgpath);
   x.setAttribute("width", "304");
   x.setAttribute("height", "228");
   x.setAttribute("alt", "Arrow Image Normal");
