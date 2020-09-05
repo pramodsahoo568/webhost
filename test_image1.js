3d12ad1d791172164304415a1f81d9e177fa6081
pramodsahoo568 <61376710+pramodsahoo568@users.noreply.github.com>
2020-09-05 19:24:13 +0530

    testimage1.html

---
+++ test_image1.js
@@ -0,0 +1,11 @@
+//var x = document.getElementsByClassName("test_image1");
+//x[0].setAttribute("src", "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg");
+
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "Arrow Image Normal");
+  
+document.getElementById("myDIV").appendChild(x);
+  
