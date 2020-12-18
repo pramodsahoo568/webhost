e21e77b06688da8ea3cc5794e991593b30000ac8
Pramod Sahoo <pramod.sahoo@calsoftinc.com>
2020-12-18 20:32:42 +0530

    modi file

--- pcs/pcstest.js
+++ pcs/pcstest.js
@@ -1,5 +1,65 @@
 
-"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"
+
+var xmlhttp_vpn = new XMLHttpRequest();
+var vpn_url = "vpn_url.txt";
+
+xmlhttp_vpn.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+	console.log(this.responseText);
+    var myArr = JSON.parse(this.responseText);
+    processVpnUrl(myArr);
+  }
+};
+xmlhttp_vpn.open("GET", vpn_url, true);
+xmlhttp_vpn.send();
+
+function processVpnUrl(arr) {
+  console.log("processVpnUrl");
+  var out = "";
+  var i;
+  for(i = 0; i < arr.length; i++) {
+    var directUrl ="";
+   try {
+    directUrl=  DanaOrigUrl(arr[i].url); 
+   } catch (error) {
+     console.log("vpn URL:"+arr[i].url);
+     console.log("Error:" + error);
+   }
+   console.log("Direct URL:"+ directUrl);
+   
+  }
+  //document.getElementById("id01").innerHTML = out;
+  
+}
+
+
+var xmlhttp_direct = new XMLHttpRequest();
+var direct_url = "direct_url.txt";
+
+xmlhttp_direct.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+	console.log(this.responseText);
+    var myArr = JSON.parse(this.responseText);
+    processDirectUrl(myArr);
+  }
+};
+xmlhttp_direct.open("GET", direct_url, true);
+xmlhttp_direct.send();
+
+function processDirectUrl(arr) {
+console.log("processDirectUrl");
+  var out = "";
+  var i;
+  /*for(i = 0; i < arr.length; i++) {
+    out += '<a href="' + arr[i].url + '">' + 
+    arr[i].display + '</a><br>';
+  }
+  document.getElementById("id01").innerHTML = out;
+  */
+}
+
+
+
 var myObj, x;
 myObj = {"name":"Path", "filename":"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"};
 filepath = myObj.filename;
@@ -20,3 +80,4 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "" + randomInt(1,100);;
 }
 
+var winpath = window.location
