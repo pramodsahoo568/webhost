80651d0eeeb45dfbfa2c6e26a626a5189d160c34
Pramod Sahoo <pramod.sahoo@calsoftinc.com>
2020-12-18 07:08:56 +0530

    added pcs files

---
+++ pcs/pcstest.js
@@ -0,0 +1,22 @@
+
+"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"
+var myObj, x;
+myObj = {"name":"Path", "filename":"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"};
+filepath = myObj.filename;
+
+var a = document.getElementById('link2'); //or grab it by tagname etc
+a.href = "https://in.yahoo.com/?p=us";
+
+var b = document.getElementById('linkimage1'); //or grab it by tagname etc
+b.href = filepath;
+
+
+
+function randomInt(min, max) {
+	return min + Math.floor((max - min) * Math.random());
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "" + randomInt(1,100);;
+}
+
