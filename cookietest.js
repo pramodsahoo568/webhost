a92fc099016e395562aff146b65dba7097c63963
pramodsahoo568 <iampramod568@gmail.com>
2022-01-27 21:01:05 +0530

    cookietest file

---
+++ cookietest.js
@@ -0,0 +1,32 @@
+
+"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"
+var myObj, x;
+myObj = {"name":"Path", "filename":"https://pramodsahoo568.github.io/webhost/dropdown_arrow_normal.svg"};
+filepath = myObj.filename;
+
+var a = document.getElementById('link2'); //or grab it by tagname etc
+a.href = "/webhost/index1.html";
+
+var b = document.getElementById('linkimage1'); //or grab it by tagname etc
+b.href = filepath;
+
+//Access_Token={"User_ID":"129892","Token":"2459592165236-1022959-37688923"}
+
+
+function randomInt(min, max) {
+	return min + Math.floor((max - min) * Math.random());
+}
+
+function myFunction() {
+	
+	var date = new Date();
+	date.setTime(date.getTime() + (2 * 60 * 60 * 1000));
+	var expires = date.toGMTString();
+	name='Access_Token';
+	value='{"User_ID":"129892","Token":"2459592165236-1022959-37688923"}'
+    console.log("1:"+document.cookie);
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+	console.log("2:"+document.cookie);
+    document.getElementById("demo").innerHTML = "" + "Cookie is Set";;
+}
+
