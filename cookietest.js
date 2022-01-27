0b541e91750b2f5b3b4f91af7b4c86fa48a75a1f
pramodsahoo568 <iampramod568@gmail.com>
2022-01-27 23:06:52 +0530

    cookietest file

--- cookietest.js
+++ cookietest.js
@@ -40,7 +40,7 @@ function myRedirect() {
 	name='Access_Token';
 	value='{"User_ID":"129892","Token":"2459592165236-1022959-37688923"}'
     console.log("1:"+document.cookie);
-    document.cookie = name + "=" + (value || "") + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 	console.log("2:"+document.cookie);
     document.getElementById("demo2").innerHTML = "" + "Cookie is Set";
 	window.location.replace("/webhost/cookie_redirect1.html");
